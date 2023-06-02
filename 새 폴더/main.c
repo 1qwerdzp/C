850430aab3146bfea718eea8498e92063891c2a4
1qwerdzp <120164884+1qwerdzp@users.noreply.github.com>
2023-06-02 17:50:26 +0900

    Add files via upload

---
+++ 새 폴더/main.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main( )
+{
+	printf("      ★★★★\n");
+	printf("       ★★★\n");
+	printf("  ★★★★★★★★\n");
+	printf("    ★★★★★★\n");
+	printf("     ★★★★★\n");
+	printf("    ★★    ★★\n");
+	printf("   ★          ★\n");
+ }
+ 
+/*
+void main( )
+{
+	printf("%%SolaTracking System%%\n");
+	printf("\\Smart Devices\\\n");
+	printf("\"Matrix\"\n"); 
+	printf("\'Hello Robot\'\n");
+ }
+*/
+
+/*
+void main( )
+{
+	printf("12345678\n");
+	printf("ABCDEFG\n");
+	printf("대한민국\n"); 
+	printf("abcdefg\n");
+	printf("▣◎♣♥\n");
+	printf("@#$^&*!\n");
+	printf("12345678\n");
+ }
+*/
+/*
+void main( )
+{
+	printf("\'");
+	printf("\\");
+	printf("\"");
+	printf("%%");
+ }
+*/
+/*
+void main( )
+{
+	printf("hello\nC3cobing");
+	printf("hello\bC3cobing");
+	printf("hello\rC3cobing");
+	printf("hello\tC3cobing");
+ }
+*/
+
+/*
+void main( )
+{
+	printf("youngil\n");
+	printf("2학년5반\n");
+	printf("이시후\n");
+ }
+ */ 
+/* run this program using the console pauser or add your own getch, system("pause") or input loop 
+
+int main(int argc, char *argvs[]) 
+{
+	printf("안녕! 씨큐브코딩");
+	return 0;
+}
+*/
+/*
+#include <stdio.h>
+
+int main( )
+{
+	printf("씨큐브코딩은");
+	printf("내일을 위해 사과나무를 심는 것이다.");
+	return 0; 
+ } 
+ */
+ 
+ 
+/*
+#include <stdio.h>
+
+int main( )
+{
+	printf("씨큐브코딩은");
+	printf("보다 나은 세상을 만드는 도구이다.");
+ } 
+ */
