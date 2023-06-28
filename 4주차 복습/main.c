bcc64a14f552f60bd5509baa14871e77e8da1d15
1qwerdzp <120164884+1qwerdzp@users.noreply.github.com>
2023-06-28 21:20:21 +0900

    Add files via upload

---
+++ 4주차 복습/main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+/*int main(int argc, char *argv[]) {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d×%d=%d\n%d÷%d=%d\n%d＋%d=%d\n%d－%d=%d\n", a, b, a*b, a, b, a/b, a, b, a+b, a, b, a-b);
+	float c, d;
+	scanf("%f %f", &c, &d);
+	printf("%f×%f=%f\n%f÷%f=%f\n%f+%f=%f\n%f-%f=%f\n", c, d, c*d, c, d, c/d, c, d, c+d, c, d, c-d);
+}*/
+/*
+void main(){
+	printf("%d\n", 10/3);
+	printf("%d\n", 10/3.0);
+	printf("%f\n", 10/3.0);
+}*/
+/*void main() {
+	int a, b;
+	
+	a, b=12, 23;
+	printf("%d\n", 12+23);
+	printf("%d\n", a+23);
+	printf("%d", a+b);
+}*/
+
+/*void main() {
+	int a=70, b=7;
+	float f1=77.0, f2=7.0;
+	
+	printf("%d\n", a/b);
+	printf("%f\n", f1/b);
+	printf("%f\n", a/f2);
+	printf("%f\n", f1/f2);
+}*/
+
+/*void main() {
+	int a, b, c;
+	
+	printf("%정수입력:");
+	scanf("%d", &a);
+	printf("정수입력:");
+	scanf("%d", &b);
+	printf("%d\n", 10%3);
+	printf("%d\n", a%10);
+	c= a%b;
+	printf("%d", c);
+}*/
+
+void main() {
+	int dlf, qm;
+	int wjd, eo, rud, dlfdks, qmdks, dlfrl, qmrl;  
+	printf("일반인의 수:");
+	scanf("%d", &dlf);
+	printf("VIP의 명수:");
+	scanf("%d", &qmdl);
+	/*안내 스태프는 1시간 30분 일하며 공연 30분 전 도움/4시간 공연/ 
+	printf("일반석의 수는 \n", 10%3);
+	printf("%d\n", a%10);
+	c= a%b;
+	printf("%d", c);
+}
+
+
