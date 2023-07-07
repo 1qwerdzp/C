6ba1e9a24620b82707460d93004d26011a971220
1qwerdzp <120164884+1qwerdzp@users.noreply.github.com>
2023-07-07 17:50:22 +0900

    Add files via upload

---
+++ 6 주차/main.c
@@ -0,0 +1,164 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+/*int main(int argc, char *argv[]) {
+	int a=20;
+	
+	a=a+10;
+	printf("a=%d\n", a);
+	a=a*10;
+	printf("a=%d\n", a);
+	a=a-10;
+	printf("a=%d\n", a);
+	a=a/10;
+	printf("a=%d\n", a);
+	return 0;
+}*/
+
+/*void main(){
+	int a,b;
+	
+	a=20;
+	b=5;
+	
+	a+=5;
+	
+	a+=b;
+	printf("a=%d\n",a);
+	a*=b;
+	printf("a=%d\n",a);
+	a-=b;
+	printf("a=%d\n",a);
+	a/=b;
+	printf("a=%d\n",a);
+	a%=b;
+	printf("a=%d\n",a);
+}*/
+/*void main(){
+	int a=10;
+	
+	a++;
+	a++;
+	printf("a=%d\n", a++);
+	++a;
+	++a;
+	printf("a=%d\n", ++a);
+	
+	a=10;
+	
+	a--;
+	printf("a=%d\n", a);
+	--a;
+	printf("a=%d\n", a);
+}*/
+/*void main(){
+	int a,b;
+	b=2;
+	
+	a=b++;
+	printf("a=%d\n",a);
+	a=++b;
+	printf("a=%d\n",a);
+}*/
+/*void main(){
+	int num1=10;
+	int num2=20;
+	
+	int num3=++num1+num2++;
+	
+	printf("a=%d\n", num1);
+	printf("a=%d\n", num2);
+	printf("a=%d\n", num3);
+}*/
+/*void main(){
+	int a=20;
+	
+	a+=4;
+	printf("a=%d\n", a);
+	a/=2;
+	printf("a=%d\n", a);
+	a--;
+	printf("a=%d\n", a);
+	a=a*11;
+	printf("a=%d\n", a);
+}*/
+/*void main(){
+	int a=10;
+	int b=2;
+	
+	a-=b;
+	printf("a=%d\n", a);
+	a*=b;
+	printf("a=%d\n", a);
+	a--;
+	printf("a=%d\n", a);
+	a%=b;
+	printf("a=%d\n", a);
+	a+=b;
+	printf("a=%d\n", a);
+	a++;
+	printf("a=%d\n", a);
+	a/=b;
+	printf("a=%d\n", a);
+}*/
+/*void main(){
+	int a, b;
+	
+	a=10;
+	b=a++;
+	printf("a=%d b=%d\n", a, b);
+	b=++a;
+	printf("a=%d b=%d\n", a, b);
+	
+	a=20;
+	b=a--;
+	printf("a=%d b=%d\n", a, b);
+	b=--a;
+	printf("a=%d b=%d\n", a, b);
+}*/
+/*void main(){
+	int num=3;
+	
+	printf("%d\n", num-=1);
+	printf("%d\n", --num);
+	printf("%d\n", num--);
+	printf("%d\n", num);
+}*/
+/*void main(){
+	int num=45;
+	
+	printf("num=45\n");
+	num=(num+1*3)-5*2-1*2;
+	printf("%d\n\n", num);
+	
+	num=47;
+	printf("num=4\n");
+	num=(num+5+1+5)-1*2-5;
+	printf("%d\n", num);
+}*/
+/*void main(){
+	int a=12;
+	int b=3;
+	
+	a-=b++;
+	printf("a=%d b=%d\n", a, b);
+	a/=--b;
+	printf("a=%d b=%d\n", a, b);
+	a+=b++;
+	printf("a=%d b=%d\n", a, b);
+	a-=++b;
+	printf("a=%d b=%d\n", a, b);
+	a+=--b;
+	printf("a=%d b=%d\n", a, b);
+}*/
+/*void main(){
+	int a, h, m, s;
+	scanf("%d", &a);
+	
+	s=a%60;
+	m=(a/60)%60;
+	h=a/3600;
+	printf("%d:%d:%d", h, m, s);
+}*/
