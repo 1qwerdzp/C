7f19bac54db4f02b833b1b4f506f3225fd6359e0
1qwerdzp <120164884+1qwerdzp@users.noreply.github.com>
2023-07-11 18:57:05 +0900

    Add files via upload

---
+++ 6 상상/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+/*int main(int argc, char *argv[]) {
+	int a, b, c, d, e, f, g, h, i;
+	printf("쉼표로 구분해서 숫자 대입:");
+	scanf("%d, %d, %d, %d", &a, &b, &c, &d);
+	e=a>b;
+	f=b>c;
+	g=c>d;
+	h=a>d;
+	if (e+f+g==3){
+		if (h==1){
+			printf("%d", (1000*b+100*a+10*c+d))-(1000*a+100*c+10*b+d);
+		}
+		else {
+			printf
+		}
+	}
+	return 0; }*/
+	
+void main(){
+	int a, b, c, d, e;
+	printf("쉼표로 구분해서 숫자 대입:");
+	scanf("%d, %d, %d, %d", &a, &b, &c, &d);
+	20
+	printf("%d")
+}
