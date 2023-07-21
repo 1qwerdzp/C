5b1fa912f6c6d9901c465e93105941e6abfe2de1
1qwerdzp <120164884+1qwerdzp@users.noreply.github.com>
2023-07-21 17:49:13 +0900

    Add files via upload

---
+++ C 8주차/main.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+/*int main(int argc, char *argv[]) {
+	int i;
+	
+	for(i=1; i<=10; i++)
+	{
+		printf("씨큐브코딩%d\n", i);
+	}
+	
+	return 0;
+}*/
+/*void main()
+{
+	int n;
+	
+	for(n=-10; n<=-1; n++)
+	{
+		printf("n=%d\n", n);
+	}
+	
+	printf("마지막 n=%d", n);
+}*/
+/*void main()
+{
+	int n;
+	
+	for(n=30; n>20; n--)
+	{
+		printf("n=%d\n", n);
+	}
+	
+	printf("마지막 n=%d", n);
+}*/
+/*void main()
+{
+	int i;
+	
+	for(i=0; i<10; i++)
+	{
+		printf("i=%d\n", i);
+	}
+	
+	printf("마지막 i=%d", i);
+}*/
+/*void main()
+{
+	for(int i=0; i<10; i++)
+	{
+		printf("i=%d\n", i);
+	}
+	
+	printf("마지막 i=%d", i);
+}*/
+
+/*void main(){
+	int i, n;
+	for(i=0; i<10; i++){
+		n=i%2;
+		printf("%d", 1-2*(n));
+	}
+}*/
+/*void main(){
+	int i;
+	
+	for(i=0; i<10; i++){
+		printf("%d", 1-i%2);
+	}
+}*/
+/*void main(){
+	int i, n;
+	
+	for(i=0; i<10; i++){
+		n=i%2;
+		printf("%d", 1-2*(n));
+	}
+}*/
+/*void main(){
+	int i, n, a=0;
+	scanf("%d", &n);
+	for(i=1; i<=n; i++){
+		a+=i;
+	}
+	printf("%d", a);
+}*/
