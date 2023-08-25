2246a835fe38f91deac554a1c3c762bb47da4a6f
1qwerdzp <120164884+1qwerdzp@users.noreply.github.com>
2023-08-25 17:47:19 +0900

    Add files via upload

---
+++ C 12주차/main.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+/*int add(int num1, int num2);
+int min(int num1, int num2);
+int mul(int num1, int num2);
+double divi(int num1, int num2);
+
+int main(int argc, char *argv[]) {
+	int num1, num2;
+	scanf("%d, %d", &num1, &num2);
+	printf("%d, %d, %d, %lf", add(num1, num2), min(num1, num2), mul(num1, num2), divi(num1, num2));
+	return 0;
+}
+
+int add(int num1, int num2){
+	return num1+num2;
+}
+
+int min(int num1, int num2){
+	return num1-num2;
+}
+
+int mul(int num1, int num2){
+	return num1*num2;
+}
+
+
+double divi(int num1, int num2){
+	return 1.0*num1/num2;
+}*/
+/*int Sum(int n);
+
+void main(){
+	int n;
+	scanf("%d", &n);
+	printf("%d", Sum(n));
+}
+
+int Sum(int n){
+	int a=0;
+	for (n; n>=1; n--){
+		a+=n;
+	}
+	return a;
+}*/
+/*void Divisor(int n);
+
+void main(){
+	int n;
+	scanf("%d", &n);
+	Divisor(n);
+	return 0;
+}
+
+void Divisor(int n){
+	int s;
+	for (s=1; s<=n; s++){
+		if (n%s==0){
+			printf(" %d", s);
+		}
+	}
+}*/
+/*int ReNum(int n);
+
+void main(){
+	int n;
+	scanf("%d", &n);
+	printf("%d", ReNum(n));
+	return 0;
+}
+
+int ReNum(int n){
+	int a=1, x=0, i;
+	for(i=1; i<n/10; i*=10){
+	}
+	while (1){
+		x+=(n/i)*a;
+		n%=i;
+		a*=10;
+		i/=10;
+		if(i<1){
+			break;
+		}
+	}
+	return x;
+}*/
+/*int AV(int n);
+
+void main(){
+	int n;
+	scanf("%d", &n);
+	printf("%d", AV(n));
+	return 0;
+}
+
+int AV(int n){
+	if (n<0){
+		n-=n*2;
+	}
+	return n;
+}*/
