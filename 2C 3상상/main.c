c2e2a2d14efdc18637fda4934824af983169c0c3
1qwerdzp <120164884+1qwerdzp@users.noreply.github.com>
2023-09-26 18:30:11 +0900

    Add files via upload

---
+++ 2C 3상상/main.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input*/
+/*
+void An(int, int);
+void Sn(int*, int*);
+void Mn(int*, int*);
+void Dn(int, int);
+
+void main(){
+	int a, b;
+	int *pa=&a, *pb=&b;
+	
+	printf("정수입력:");
+	scanf("%d, %d", &a, &b);
+	
+	An(a, b);
+	Sn(&a, &b);
+	Mn(pa, pb);
+	Dn(*pa, *pb);
+}
+
+void An(int a, int b){
+	printf("%d ＋ %d = %d", a, b, a+b);
+}
+void Sn(int *pa, int *pb){
+	printf("\t%d - %d = %d", *pa, *pb, *pa-* pb);
+}
+void Mn(int*pa, int*pb){
+	printf("\t%d × %d = %d", *pa, *pb, *pa**pb);
+}
+void Dn(int pa, int pb){
+	printf("\t%d ÷ %d = %d", pa, pb, pa/pb);
+}
+*/
+
+/*void II(int*);
+void ID(double*);
+void PI(int*);
+void PD(double*);
+void CI(int*);
+void CD(double*);
+
+int main(){
+	int n;
+	double d;
+	II(&n);
+	ID(&d);
+	PI(&n);
+	PD(&d);
+	CI(&n);
+	CD(&d);
+	PI(&n);
+	PD(&d);
+	return 0;
+}
+
+void II(int *n){
+	printf("정수 입력 : ");
+	scanf("%d",n);
+}
+void ID(double *d){
+	printf("실수 입력 : ");
+	scanf("%lf", d);
+}
+void PI(int *n){
+	printf("정수=> : %d\n", *n);
+}
+void PD(double *d){
+	printf("실수=> : %lf\n", *d);
+}
+void CI(int *n){
+	*n+=10;
+}
+void CD(double *d){
+	*d*=10;
+}*/
+/*int input();
+
+int main(){
+	int n, m, t;
+	
+	m=input();
+	m=input();
+	printf("Before: ");
+	printf("n=%d m=%d\n", n, m);
+	return 0;
+}
+
+int input(){
+	int a;
+	
+	printf("정수입력:");
+	scanf("%d", &a);
+	return a;
+}*/
+/*void input(int *);
+void SD(int *, int*);
+
+int main(){
+	int n, m;
+	
+	input(&n);
+	input(&m);
+	printf("Before: ");
+	printf("n=%d m=%d\n", n, m);
+	SD(&n, &m);
+	printf("After:");
+	printf("n=%d m=%d\n", n, m);
+	return 0;
+}
+
+void input(int *a){
+	printf("정수입력:");
+	scanf("%d", a);
+}
+void SD(int *c, int *d){
+	int a=*c;
+	*c=*d;
+	*d=a;
+}*/
